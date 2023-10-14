e672374a9b708bc08b7496ab100eff1bc5013225
João Victor Camargo Silva <joao.victor@globalsys.com>
2023-10-14 08:05:09 -0300

    feat: FileUpload

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
+import { InitialModal } from "@/components/modals/initial-modal";
 // import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
@@ -23,7 +24,7 @@ const SetupPage = async () => {
 
 //   return <InitialModal />;
 return (
-    <div>Create a Server</div>
+    <InitialModal />
 )
 }
  
